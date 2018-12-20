82f9f00821f155db09d766c21a5c568d650c9363
lubodrinka <lubodrinka@gmail.com>
2018-12-20 23:39:51 +0100

    Updates

---
+++ precache-manifest.c9085df15d6b4897fd6d05993c2b436f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Personal-Portfolio-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8428f83e20e11665552b01d4d40a147",
+    "url": "/Personal-Portfolio-react/static/media/flags.f8428f83.png"
+  },
+  {
+    "revision": "5dd65c0e64f28023df46",
+    "url": "/Personal-Portfolio-react/static/js/runtime~main.5dd65c0e.js"
+  },
+  {
+    "revision": "2ce7bb7315965dc3925b",
+    "url": "/Personal-Portfolio-react/static/js/main.2ce7bb73.chunk.js"
+  },
+  {
+    "revision": "1500afdb8401cf6262ae",
+    "url": "/Personal-Portfolio-react/static/js/1.1500afdb.chunk.js"
+  },
+  {
+    "revision": "2ce7bb7315965dc3925b",
+    "url": "/Personal-Portfolio-react/static/css/main.bc542964.chunk.css"
+  },
+  {
+    "revision": "28326a57927737423b37c1daba224591",
+    "url": "/Personal-Portfolio-react/index.html"
+  }
+];
