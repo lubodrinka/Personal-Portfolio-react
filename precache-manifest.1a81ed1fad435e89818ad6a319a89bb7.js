f8bb2b7677544e869000851c3e01a8fda78630cf
lubodrinka <lubodrinka@gmail.com>
2018-12-21 02:32:55 +0100

    Updates

---
+++ precache-manifest.1a81ed1fad435e89818ad6a319a89bb7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2843d633b3e5ccc3e51708a7aa84c99",
+    "url": "/Personal-Portfolio-react/static/media/leaf4.a2843d63.png"
+  },
+  {
+    "revision": "2bc45c0e3b884ca38516",
+    "url": "/Personal-Portfolio-react/static/css/main.c8011485.chunk.css"
+  },
+  {
+    "revision": "74cbae74d68d014362c4",
+    "url": "/Personal-Portfolio-react/static/js/1.74cbae74.chunk.js"
+  },
+  {
+    "revision": "5dd65c0e64f28023df46",
+    "url": "/Personal-Portfolio-react/static/js/runtime~main.5dd65c0e.js"
+  },
+  {
+    "revision": "a603c9eb5567730ffa70aad5824f3406",
+    "url": "/Personal-Portfolio-react/static/media/leaf.a603c9eb.png"
+  },
+  {
+    "revision": "b86a27ac56cccb71dfbfb4bd5fb7204e",
+    "url": "/Personal-Portfolio-react/static/media/leaf2.b86a27ac.png"
+  },
+  {
+    "revision": "d63b71a5af4d7aa158398b7c579163ef",
+    "url": "/Personal-Portfolio-react/static/media/leaf3.d63b71a5.png"
+  },
+  {
+    "revision": "2bc45c0e3b884ca38516",
+    "url": "/Personal-Portfolio-react/static/js/main.2bc45c0e.chunk.js"
+  },
+  {
+    "revision": "c4f0dd357e151dbb59f879d55902dcd9",
+    "url": "/Personal-Portfolio-react/static/media/leaf5.c4f0dd35.png"
+  },
+  {
+    "revision": "10a91fc70cdf22046af418f4a8b9c02a",
+    "url": "/Personal-Portfolio-react/static/media/leaf6.10a91fc7.png"
+  },
+  {
+    "revision": "319ea8a163814daa51789e913f849a3f",
+    "url": "/Personal-Portfolio-react/static/media/leaf7.319ea8a1.png"
+  },
+  {
+    "revision": "fe58bcd9d944c7797799f0b7363a2cf1",
+    "url": "/Personal-Portfolio-react/static/media/leaf8.fe58bcd9.png"
+  },
+  {
+    "revision": "f8428f83e20e11665552b01d4d40a147",
+    "url": "/Personal-Portfolio-react/static/media/flags.f8428f83.png"
+  },
+  {
+    "revision": "afaa87c688ff5ed148e461821fddf4bd",
+    "url": "/Personal-Portfolio-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Personal-Portfolio-react/precache-manifest.57318edd63bec66246aaabb33024310b.js"
+  "/Personal-Portfolio-react/precache-manifest.1a81ed1fad435e89818ad6a319a89bb7.js"
 );
 
 workbox.clientsClaim();
